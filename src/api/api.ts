ec1a373019a0eb5d6a9b8bed3072edaf5a533a61
Francis Amidu <francisamidu124@gmail.com>
2025-06-24 13:35:31 +0200

    update:Changed backend URL

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,11 @@
 import ky from "ky";
 
 const api = ky.create({
-  prefixUrl: "https://that-tracker-backend.onrender.com/api",
+  prefixUrl: "https://jvjdyvelpzuxzcfcinpp.supabase.co/functions/v1/express",
   // prefixUrl: "http://localhost:3001/api",
   headers: {
     Accept: "application/json",
+    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
     "Content-Type": "application/json",
   },
 });
